fd6bd7b3a541fb695346ad04841d8612db760d28
Martí <pardo.marti@gmail.com>
2025-12-27 00:46:04 +0100

    metadata and opengraph tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,36 @@ const geistMono = Geist_Mono({
   variable: "--font-mono"
 })
 
+export const metadata: Metadata = {
+  title: "CALENDARI",
+  description: "Calendari mensual per organitzar-se la vida.",
+  openGraph: {
+    title: "CALENDARI",
+    description: "Calendari mensual per organitzar-se la vida.",
+    url: "https://calendari.martipardo.com",
+    siteName: "CALENDARI",
+    images: [
+      {
+        url: "https://calendari.martipardo.com/banner.png",
+        width: 512,
+        height: 512,
+        alt: "Calendari logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "CALENDARI",
+    description: "Calendari mensual per organitzar-se la vida.",
+    images: ["https://calendari.martipardo.com/banner.png"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/favicon.ico",
+  }
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
